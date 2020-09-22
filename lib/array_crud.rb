53696d6f86deb818f8fe803652833cfca3104b9d
Lee Michaeli <leeisr@gmail.com>
2020-09-22 20:00:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -44,5 +44,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array = ["I", "am", "really", "learning"]
+  array[1] = "'m"
 end
