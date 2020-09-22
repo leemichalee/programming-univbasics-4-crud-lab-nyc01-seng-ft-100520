35e1aee076959436478fb5a8fa993dcc1280ca80
Lee Michaeli <leeisr@gmail.com>
2020-09-22 20:01:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,7 +39,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["I", "am", "really", "learning"]
+  array = ["I", "am", "really", "arrays!"]
   array[-1]
 end
 
